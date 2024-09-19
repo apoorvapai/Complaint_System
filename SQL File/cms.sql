4d6194297dda0fd9bd124a3d3bc3147e0577c96d
Apoorva Pai <53962427+apoorvapai@users.noreply.github.com>
2024-09-19 20:24:45 +0530

    SQL file

---
+++ SQL File/cms.sql
@@ -0,0 +1,276 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 15, 2024 at 04:00 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(259) DEFAULT NULL,
+  `mobilenumber` bigint(11) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `fullname`, `mobilenumber`, `email`, `username`, `password`, `creationDate`, `updationDate`) VALUES
+(1, 'admin', 9036022320, 'admin@gmail.com', 'admin', 'dc647eb65e6711e155375218212b3964', '2024-05-05 05:16:16', '05-05-2024 04:18:16');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL,
+  `categoryName` varchar(255) DEFAULT NULL,
+  `categoryDescription` longtext DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id`, `categoryName`, `categoryDescription`, `creationDate`, `updationDate`) VALUES
+(1, 'PC Manintanace', 'Deals with issues related to system', '2024-05-15 01:19:36', NULL),
+(2, 'Web', 'Deals with issues related to website', '2024-05-15 01:20:18', NULL),
+(3, 'Face Recognition', 'Deals with issues related to Face recognition', '2024-05-15 01:20:51', NULL),
+(4, 'Attandance', 'Deals with attendance issues ', '2024-05-15 01:21:21', NULL),
+(5, 'Network', 'Deals with server and Network issues', '2024-05-15 01:21:54', NULL),
+(6, 'VDI', '.', '2024-05-15 01:22:03', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaintremark`
+--
+
+CREATE TABLE `complaintremark` (
+  `id` int(11) NOT NULL,
+  `complaintNumber` int(11) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `remark` mediumtext DEFAULT NULL,
+  `remarkDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `complaintremark`
+--
+
+INSERT INTO `complaintremark` (`id`, `complaintNumber`, `status`, `remark`, `remarkDate`) VALUES
+(8, 2, 'in process', '..', '2024-05-15 01:47:50'),
+(9, 2, 'closed', '..', '2024-05-15 01:48:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `state`
+--
+
+CREATE TABLE `state` (
+  `id` int(11) NOT NULL,
+  `stateName` varchar(255) DEFAULT NULL,
+  `stateDescription` tinytext DEFAULT NULL,
+  `postingDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcategory`
+--
+
+CREATE TABLE `subcategory` (
+  `id` int(11) NOT NULL,
+  `categoryid` int(11) DEFAULT NULL,
+  `subcategory` varchar(255) DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcomplaints`
+--
+
+CREATE TABLE `tblcomplaints` (
+  `complaintNumber` int(11) NOT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `category` int(11) DEFAULT NULL,
+  `staffno` bigint(20) NOT NULL,
+  `dept` varchar(20) NOT NULL,
+  `iphno` bigint(20) NOT NULL,
+  `complaintDetails` mediumtext DEFAULT NULL,
+  `regDate` timestamp NULL DEFAULT current_timestamp(),
+  `status` varchar(50) DEFAULT NULL,
+  `lastUpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblcomplaints`
+--
+
+INSERT INTO `tblcomplaints` (`complaintNumber`, `userId`, `category`, `staffno`, `dept`, `iphno`, `complaintDetails`, `regDate`, `status`, `lastUpdationDate`) VALUES
+(1, 8, 1, 1234, 'Is', 9036022320, '..', '2024-05-14 17:36:02', NULL, NULL),
+(2, 9, 1, 1234, 'IS', 9036022320, '//', '2024-05-15 01:35:17', 'closed', '2024-05-15 01:48:25');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `fullName` varchar(255) DEFAULT NULL,
+  `userEmail` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `contactNo` bigint(11) DEFAULT NULL,
+  `address` tinytext DEFAULT NULL,
+  `State` varchar(255) DEFAULT NULL,
+  `country` varchar(255) DEFAULT NULL,
+  `pincode` int(6) DEFAULT NULL,
+  `userImage` varchar(255) DEFAULT NULL,
+  `regDate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updationDate` timestamp NULL DEFAULT NULL,
+  `status` int(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `fullName`, `userEmail`, `password`, `contactNo`, `address`, `State`, `country`, `pincode`, `userImage`, `regDate`, `updationDate`, `status`) VALUES
+(8, 'Apoorva Pai', 'apoorvapai047@gmail.com', 'ad2847c0069a828bb54a78eeeb2c60bf', 9036022320, NULL, NULL, NULL, NULL, NULL, '2024-05-14 17:29:53', NULL, 1),
+(9, 'Apoorva Pai', 'abc@gmail.com', '0a8cbe515dd4acde4d6d96599d1a9c22', 9036022320, NULL, NULL, NULL, NULL, NULL, '2024-05-15 01:34:43', NULL, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `complaintremark`
+--
+ALTER TABLE `complaintremark`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `state`
+--
+ALTER TABLE `state`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tblcomplaints`
+--
+ALTER TABLE `tblcomplaints`
+  ADD PRIMARY KEY (`complaintNumber`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `complaintremark`
+--
+ALTER TABLE `complaintremark`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `state`
+--
+ALTER TABLE `state`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `subcategory`
+--
+ALTER TABLE `subcategory`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblcomplaints`
+--
+ALTER TABLE `tblcomplaints`
+  MODIFY `complaintNumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
